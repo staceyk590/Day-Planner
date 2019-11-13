567c98fb7ba992ecfa1e38b8791b8421ed1a11b5
Stacey Koumaras <stacey_k@tpg.com.au>
2019-11-14 00:37:50 +1100

    homework

--- script.js
+++ script.js
@@ -20,10 +20,6 @@
 //use moment.js in browser
 
 
-//var d = moment.tz("2019-04-15 12:00", "America/New_York");
-
-//console.log( new Date(d) );
-//console.log( new Date(moment()) );
 const now = moment().startOf('hour');
 const currentHour = now.hour();
 $("#CurrentDay").text(now.format("DD MMM YYYY"));
@@ -31,41 +27,26 @@ $("#CurrentDay").text(now.format("DD MMM YYYY"));
 
 
 
-$("#container").on("click", ".Submit", function() {
-	const $parent = $(this).parent();
-
-	const hour = $parent.data('hour');
-	const userInput = $parent.find(".input").val();
-
-	planner[hour] = userInput;
-	localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(planner));
-});
 
 
 
 
-//var notepad = document.getElementById("notepad");
-//var schedule = document.getElementById("schedule");
-//console.log(schedule.innerHTML);
-//var note;
-//window.onkeyup = save;
+function supportsLocalStorage() {
+  return ('localStorage' in window) && window['localStorage'] !== null;
+}
 
-//function supportsLocalStorage() {
-  //  return ('localStorage' in window) && window['localStorage'] !== null;
-//}
+function save() {
+ if (!supportsLocalStorage()) { return false; }
+	note = notepad.value;
+		console.log(note);
+		localStorage["stored.note"] =  note;
+   return true;
+}
 
-//function save() {
- //   if (!supportsLocalStorage()) { return false; }
-//		note = notepad.value;
-//		console.log(note);
-//		localStorage["stored.note"] =  note;
- //   return true;
-//}
-
-//function resume() {
-//    if (!supportsLocalStorage()) { return false; }
-//		note = localStorage["stored.note"];
-//		if (!note) { return false; }
-//		notepad.value = note;
-//		return true;
-//}
+function resume() {
+   if (!supportsLocalStorage()) { return false; }
+		note = localStorage["stored.note"];
+		if (!note) { return false; }
+		notepad.value = note;
+		return true;
+}
